6e70547027df6c9e01d234216a3a8d6735d45db3
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-07-17 02:01:47 -0400

    Updates

---
+++ precache-manifest.b319c7f57ea6d5e8dcb85a237391d5ac.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74230cedc172da908907af4c3cab5ee0",
+    "url": "/github.com/JTLaMarre.github.io/index.html"
+  },
+  {
+    "revision": "1e91ab9d7d82f7ba55dc",
+    "url": "/github.com/JTLaMarre.github.io/static/css/main.561a7460.chunk.css"
+  },
+  {
+    "revision": "731b4b33b6b1ee62f1bb",
+    "url": "/github.com/JTLaMarre.github.io/static/js/2.8fedf7b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.com/JTLaMarre.github.io/static/js/2.8fedf7b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e91ab9d7d82f7ba55dc",
+    "url": "/github.com/JTLaMarre.github.io/static/js/main.d8727071.chunk.js"
+  },
+  {
+    "revision": "d7ee48b99fcbcf8d8172",
+    "url": "/github.com/JTLaMarre.github.io/static/js/runtime-main.c5793140.js"
+  },
+  {
+    "revision": "49d691f41a88e30d007e9218e1af68da",
+    "url": "/github.com/JTLaMarre.github.io/static/media/EmployScreen1.49d691f4.PNG"
+  },
+  {
+    "revision": "fb1bd075d9a171ecc003262f64f13e2f",
+    "url": "/github.com/JTLaMarre.github.io/static/media/LetsGuess.fb1bd075.PNG"
+  },
+  {
+    "revision": "6957237c2ffb5e398da5c88d1b916ed8",
+    "url": "/github.com/JTLaMarre.github.io/static/media/Pizza.6957237c.PNG"
+  },
+  {
+    "revision": "5c6dd9745a61eeba54a22955b6e94f09",
+    "url": "/github.com/JTLaMarre.github.io/static/media/Screenshot.5c6dd974.PNG"
+  },
+  {
+    "revision": "db182b93f6357494ab4e127d21bf8edc",
+    "url": "/github.com/JTLaMarre.github.io/static/media/Work day scheduler.db182b93.PNG"
+  },
+  {
+    "revision": "e29b57ab795d639e84044325010e5449",
+    "url": "/github.com/JTLaMarre.github.io/static/media/mePic.e29b57ab.jpg"
+  },
+  {
+    "revision": "2cc86e6e9ea63a9689f0ea248137aa90",
+    "url": "/github.com/JTLaMarre.github.io/static/media/trailheads.2cc86e6e.PNG"
+  },
+  {
+    "revision": "fa66c0dd3efc720ea14b64ebd1306e08",
+    "url": "/github.com/JTLaMarre.github.io/static/media/weatherdash.fa66c0dd.PNG"
+  }
+]);
