f57cc63d08d6f4d80864abee9e59b65e68ba9a8a
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-07-17 02:02:11 -0400

    redid and reloaded

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header';
+import Project from '../Project';
+import Footer from '../Footer';
+
+const Portfolio = (props) => {
+    return (
+        <div>
+            <Header />
+            <Project />
+            <Footer/>
+         </div>
+    );
+}
+
+export default Portfolio;
