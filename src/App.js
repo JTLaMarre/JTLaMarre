50cc927027734eecdaa6f73e0f8b150b5ac4b305
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-07-17 08:19:12 -0400

    trying the basename in browser router.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return ([      
       
-       <BrowserRouter>
+       <BrowserRouter basename={process.env.PUBLIC_URL} >
         <div>
           <Navigation />
             <Switch>
